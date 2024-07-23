20f80e0710e55b58759f6f1ccc33171e0fa52e22
jinuk <commontoday@naver.com>
2024-07-23 19:27:18 +0900

    에러 수정

--- src/main/java/cs/club/mojuk/menu/IndexController.java
+++ src/main/java/cs/club/mojuk/menu/IndexController.java
@@ -1,9 +1,8 @@
-package inhatc.group.mojuk.menu;
+package cs.club.mojuk.menu;
 
+import jakarta.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-
-import javax.servlet.http.HttpSession;
 
 @Controller
 public class IndexController {
