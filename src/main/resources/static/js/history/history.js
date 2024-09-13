59338d4eca79b4c686834a39252c06c671360a94
jinuk <commontoday@naver.com>
2024-09-13 18:31:40 +0900

    조직도 배치 정렬 추가 중

--- src/main/resources/static/js/history/history.js
+++ src/main/resources/static/js/history/history.js
@@ -1,10 +1,10 @@
 const swiper = new Swiper('.swiper-container', {
     loop: true,
-    autoplay: {
+    autoplay: false, /*{
         delay: 10000,
         disableOnInteraction: false, // 사용자가 상호작용 후에도 자동재생 유지
         pauseOnMouseEnter: true, // 마우스가 슬라이더 위에 있을 때 자동 이동 멈춤
-    },
+    },*/
 
     pagination: {
         el: '.swiper-pagination',
@@ -29,12 +29,23 @@ const swiper = new Swiper('.swiper-container', {
 
 // 데이터 생성
 const chartData = [
-    { id: 1, name: "CEO" },
-    { id: 2, pid: 1, name: "Vice President 1" },
-    { id: 3, pid: 1, name: "Vice President 2" },
-    { id: 4, pid: 2, name: "Manager 1" },
-    { id: 5, pid: 2, name: "Manager 2" },
-    { id: 6, pid: 3, name: "Manager 3" }
+    { id: 1, name: "회장" },
+    { id: 2, name: "지도교수" },
+    { id: 3, pid: 1, name: "부회장", layout: OrgChart.mixed },
+    { id: 4, pid: 1, name: "저학년부장", layout: OrgChart.mixed },
+    { id: 5, pid: 1, name: "총무" },
+    { id: 6, pid: 3, name: "회원" },
+    { id: 7, pid: 3, name: "회원" },
+    { id: 8, pid: 3, name: "회원" },
+    { id: 9, pid: 3, name: "회원" },
+    { id: 10, pid: 4, name: "회원" },
+    { id: 11, pid: 4, name: "회원" },
+    { id: 12, pid: 4, name: "회원" },
+    { id: 13, pid: 4, name: "회원" },
+    { id: 14, pid: 4, name: "회원" },
+    { id: 15, pid: 4, name: "회원" },
+    { id: 16, pid: 4, name: "회원" },
+    { id: 17, pid: 4, name: "회원" }
 ];
 
 // OrgChart 초기화
@@ -49,4 +60,5 @@ const chart = new OrgChart(document.getElementById("tree"), {
     enableSearch: false,
     nodeMouseClick: OrgChart.action.none,
     mouseScrool: OrgChart.action.none,
+    scaleInitial: 0.7
 });
