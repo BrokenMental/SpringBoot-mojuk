702cb98348dfaad48bc9cddbef72d8fe226e6d4f
jinuk-home <commontoday@naver.com>
2024-12-29 03:58:42 +0900

    연도 데이터 불러오기 작업 중

--- src/main/resources/static/js/history/history.js
+++ src/main/resources/static/js/history/history.js
@@ -38,27 +38,59 @@ const swiper = new Swiper('.swiper-container', {
     */
 });
 
+//오리지널 슬라이드 복사
+/*
 const originalSlides = Array.from(document.querySelectorAll('.swiper-slide')).map(slide => {
-  return {
-    content: slide.innerHTML
-  };
+    return {
+        content: slide.innerHTML
+    };
 });
+*/
 
 const manageYearList = async () => {
     try {
         const response = await axios.get('/history/manageYears');
         const years = response.data.map(yearList => yearList.manageYear);
 
+        // 페이지네이션 불릿에 연도 데이터 추가
+        //const bullets = document.querySelectorAll('.swiper-pagination .swiper-pagination-bullet');
+        /*bullets.forEach((bullet, index) => {
+            bullet.textContent = years[index];
+        });*/
+
         // Swiper 슬라이드 업데이트
         swiper.removeAllSlides();
         years.forEach((year, index) => {
+            // swiper-slide elements 클래스 하위에 기존에 저장된 자식 요소가 있다면 복원
+            /*
             swiper.appendSlide(`<div class="swiper-slide">Slide ${index + 1}</div>`);
 
-            // 기존에 저장된 자식 요소가 있다면 복원
             if (originalSlides[index]) {
                 const newSlide = swiper.slides[swiper.slides.length - 1];
                 newSlide.innerHTML = originalSlides[index].content;
             }
+            */
+
+            //swiper-slide 클래스 elements 하위 요소 추가
+            swiper.appendSlide(`<div class="swiper-slide">
+<div class="swiper-slide-header">Slide ${index + 1}</div>
+<div class="swiper-slide-body">
+    <div id="usersContainer">
+        <input class="search" placeholder="검색" />
+        <div>
+            <span>정렬</span>
+            <button class="sort" data-sort="name">이름</button>
+            <button class="sort" data-sort="studentNumber">입학년도</button>
+            <button class="sort" data-sort="clubLevel">직무</button>
+        </div>
+        <ul class="list"></ul>
+    </div>
+</div>`);
+
+            //여기서 작업할 경우 루프를 돌 때 페이지네이션이 생성 되면서 초기화 되어 기존에 입력된 데이터가 사라짐
+            const bullets = document.querySelectorAll('.swiper-pagination .swiper-pagination-bullet');
+            if(index > 0) bullets[index].textContent = years[index];
+
         });
 
         // 페이지네이션 업데이트
@@ -69,12 +101,6 @@ const manageYearList = async () => {
         //swiper.loopCreate();
 
         swiper.pagination.update();
-
-        // 페이지네이션 불릿에 연도 데이터 추가
-        const bullets = document.querySelectorAll('.swiper-pagination .swiper-pagination-bullet');
-        bullets.forEach((bullet, index) => {
-            bullet.textContent = years[index];
-        });
 
         clubDisplay();
     } catch (err) {
